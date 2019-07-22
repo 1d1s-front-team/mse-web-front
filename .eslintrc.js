2345af7ddeaa1db89e15b49625141e07d17a012d
Hyewon Park <changeling626@gmail.com>
2019-07-22 15:47:24 +0900

    ESLINT error solved

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    root: true,
+    extends: [
+        'plugin: vue/essential'
+    ],
+    parserOptions: {
+        parser: 'babel-eslint',
+        ecmaVersion: 2017,
+        sourceType: 'module'
+    },
+    plugins: [
+        'vue'
+    ],
+    env: {
+        browser: true
+    },
+    rules: {
+    }
+}
